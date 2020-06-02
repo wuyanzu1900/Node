c55becb6a78c511ee6a61c8f54b72af5f022a64e
zm <1109282075@qq.com>
2020-06-02 10:07:48 +0800

    读取JSON

---
+++ cache-file/app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const users = require('./users.json');
+// console.log(users);
+let version = 1;
+let server = http.createServer((req, res) => {
+  // If-None-Match
+  if (req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    fs.createReadStream('index.html').pipe(res);
+  } else if (req.url == '/users') {
+    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    // send 文本  Buffer 
+    // JSON 对象变成文本
+    // console.log(typeof JSON.stringify(users));
+    // res.setHeader('ETag', version);
+    res.end(JSON.stringify(users)) // 怎么把JSON 传过去? 
+  }
+});
+
+server.listen(3000);
---
+++ cache-file/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req, res) => {
+  // console.log(req);
+  let stat = fs.statSync('./a.txt');
+  if (req.headers['if-modified-since']) {
+    console.log(typeof req.headers['if-modified-since'] );
+    console.log(typeof stat.mtime)
+    if (req.headers['if-modified-since'] == stat.mtime) {
+      res.statusCode = 304
+      res.end();
+      return;
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+      res.end('修改了');
+      return;
+    }
+  }
+   // 同步, 阻塞
+  res.setHeader('Last-Modified', stat.mtime) //响应头
+  // fs.createReadStream('a.txt').pipe(res);
+  res.end('hello')
+})
+.listen(5200)
